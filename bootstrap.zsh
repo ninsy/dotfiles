ee4e95f61cad194d352be54e1277656ebce05ac9
ninsy <kwojciechowski094@gmail.com>
2018-09-29 01:49:11 +0200

    initial commit with base setup

---
+++ bootstrap.zsh
@@ -0,0 +1,14 @@
+#!/usr/bin/env zsh
+
+sudo -v
+
+export DOTFILES=${DOTFILES:="$HOME/.dotfiles"}
+
+cd $DOTFILES/scripts
+
+. ./node.zsh
+. ./golang.zsh
+. ./rust.zsh
+. ./ngrok.zsh
+. ./postman.zsh
+. ./vscode.zsh
