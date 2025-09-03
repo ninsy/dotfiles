eea6a6b1e84dd8ad8da4f84636e2b0a1d1f202e9
ninsy <kwojciechowski094@gmail.com>
2025-09-03 15:21:48 +0200

    feat: fzf-show, todo notes

---
+++ git/.git-scripts/fzf-show.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# NOTE: whole '.git-scripts' dir is expected to be stored at $HOME
+# TODO: all scripts/script-like stuff to be moved into .git-scriots
+# TODO: startup script within 'git' directory that creates symbolic links at $HOME
+# TODO: overall, need such 'startup'/sync strategy...
+git log --oneline --abbrev=10 | fzf --preview='git show --color=always $(echo {} | cut -d" " -f1)' --preview-window=right:75%
