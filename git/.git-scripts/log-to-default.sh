d3b507b11d97b34f3df01d58518189dff605a52c
ninsy <kwojciechowski094@gmail.com>
2025-10-24 18:41:54 +0200

    fix: fix ld script

--- git/.git-scripts/log-to-default.sh
+++ git/.git-scripts/log-to-default.sh
@@ -4,7 +4,23 @@ set -e
 set -u
 
 # TODO: shellcheck only supports bin/bash...
-# TODO: compare with origin/HEAD instead?q
+# TODO: compare with origin/HEAD instead?
 GIT_SCRIPT_DIR="${0:A:h}"
+
+curr_branch=$(git rev-parse --abbrev-ref HEAD)
 default_branch=$(${GIT_SCRIPT_DIR}/default-branch.sh)
+
+set +e
+git stash -u
+set -e
+
+
+git co "$default_branch"
+git pull
+git co "$curr_branch"
+
+set +e
+git stash pop
+set -e
+
 git log "$default_branch"...HEAD --oneline --left-right
