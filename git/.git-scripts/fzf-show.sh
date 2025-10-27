48ed46e9cf9a3e728ae3931d1d074170e21ecba9
ninsy <kwojciechowski094@gmail.com>
2025-10-27 16:50:36 +0100

    chore: todo note

--- git/.git-scripts/fzf-show.sh
+++ git/.git-scripts/fzf-show.sh
@@ -3,4 +3,6 @@
 # TODO: all scripts/script-like stuff to be moved into .git-scriots
 # TODO: startup script within 'git' directory that creates symbolic links at $HOME
 # TODO: overall, need such 'startup'/sync strategy...
+
+# TODO: accept arg and use relative to given ref?
 git log --oneline --abbrev=10 | fzf --preview='git show --color=always $(echo {} | cut -d" " -f1)' --preview-window=right:75%
