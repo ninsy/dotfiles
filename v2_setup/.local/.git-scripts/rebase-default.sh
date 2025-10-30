b3b3451dfc79839118d2a0d58c82c385c08de9c5
ninsy <kwojciechowski094@gmail.com>
2025-10-30 12:00:04 +0100

    chore: todo notes for rebase-default fixes

--- v2_setup/.local/.git-scripts/rebase-default.sh
+++ v2_setup/.local/.git-scripts/rebase-default.sh
@@ -9,11 +9,12 @@ GIT_SCRIPT_DIR="${0:A:h}"
 curr_branch=$(git rev-parse --abbrev-ref HEAD)
 default_branch=$(${GIT_SCRIPT_DIR}/default-branch.sh)
 
+# TODO: stash only if there are some changes to existing/there are new files
 git stash -u
 git pull origin "$curr_branch"
 git co "$default_branch"
 git pull
 git co "$curr_branch"
 git rebase "$default_branch"
-# TODO: only latest stash to be applied?
+# TODO: only apply if was stashed before
 git stash pop
