39381d6ff4385d0557dfe0defb00a494b27f28a4
ninsy <kwojciechowski094@gmail.com>
2025-11-02 19:46:25 +0100

    fix: stash only if there are things to be

--- v2_setup/.local/.git-scripts/rebase-default.sh
+++ v2_setup/.local/.git-scripts/rebase-default.sh
@@ -9,12 +9,17 @@ GIT_SCRIPT_DIR="${0:A:h}"
 curr_branch=$(git rev-parse --abbrev-ref HEAD)
 default_branch=$(${GIT_SCRIPT_DIR}/default-branch.sh)
 
-# TODO: stash only if there are some changes to existing/there are new files
-git stash -u
+HAS_STASHED="0"
+if [[ -n "$(git status -s)" ]]; then
+    git stash -u
+    HAS_STASHED="1";
+fi
 git pull origin "$curr_branch"
 git co "$default_branch"
 git pull
 git co "$curr_branch"
 git rebase "$default_branch"
-# TODO: only apply if was stashed before
-git stash pop
+if [[ $HAS_STASHED == "1" ]]; then
+    git stash pop
+fi
+
